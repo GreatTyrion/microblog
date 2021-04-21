e7674d6c577b9e855fd8abc4a722225386beec7b
hqiu <hqiu@openiii.com>
2021-04-21 14:51:37 -0230

    Add Email support

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -14,6 +15,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['qiuhongyuan0420@gmail.com']
 
     POSTS_PER_PAGE = 25
